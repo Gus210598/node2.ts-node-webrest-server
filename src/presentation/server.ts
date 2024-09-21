1fbc0ccb9ad4e0da8586f9c7ed5cd0244d228105
Gustavo Barahona <gustavo.barahona.i@gmail.com>
2024-09-21 02:25:49 -0300

    Fin sección 14

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import path from 'path';
+import { Router } from 'express';
 
 interface Options {
     port        : number;
     public_path?: string;
+    routes       : Router;
 }
 
 export class Server {
@@ -12,20 +14,35 @@ export class Server {
 
     private readonly port: number;
     private readonly publicPath: string;
+    private readonly routes: Router;
 
     constructor( options: Options ) {
-        const { port, public_path = 'public' } =  options;
+        const { port, routes, public_path = 'public' } =  options;
         this.port       = port;
-        this.publicPath = public_path
+        this.publicPath = public_path;
+        this.routes = routes;
     }
 
     async start(){
 
+        
         //* Middlewares
+        //! al usar "use" son los middlewares
+        this.app.use( express.json() ); // row
+        this.app.use( express.urlencoded({ extended: true }) ); // x-www-form-urlencoded
+
+
+        //* Routes
+        this.app.use( this.routes );
 
         //* Public Folder
         this.app.use( express.static( this.publicPath ) );
 
+       
+
+
+
+        //* comodin cualquier ruta no definida pasa por acá SPA
         this.app.get('*', ( req, res ) => {
             // console.log(req.url);
             // res.send('Hi desde cualquier ruta');
